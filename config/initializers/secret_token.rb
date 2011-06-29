88f48641539c7eaa5c5f40415e53f36afe1ebe5a
Tony Wang <tonyjwang@gmail.com>
2011-06-28 21:45:47 -0600

    ~initial.~

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Ridemo::Application.config.secret_token = '162f53e6056247e6cff5fccaa27454ab36572ab4e0403af167c0d4d34eb9850350d426b457f6e2ffc57bceaa3d7657e1cebbf1229408d71410465d547ad74940'
+Ridemo::Application.config.secret_token = '26ecec3620298cbaf957b1a62f103525b4d570d744965f2ee7c35a11d21e9c1e7a48501db70e9a2d0121ed4abcde89df6a47d9cf499aeed47103c0dfefd21857'
